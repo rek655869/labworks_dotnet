77873ccf1d28694fb9cac7d563fcae3e4d54cf01
rek655689 <e.gladneva1357@gmail.com>
2023-10-11 01:17:46 +0300

    задание 2 для 5 лабы

---
+++ LW_05_ex2_console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LW_05_ex2_console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LW_05_ex2_console")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("385aeaf0-6187-4f88-b566-e9d4da6b8872")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LW_05_ex2_forms/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LW_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] words = textBox1.Text.Remove(textBox1.Text.Length - 1).Split(' ');
+            if (words.Length < 2 || words.Length > 30 )
+            {
+                MessageBox.Show(this,
+                        "Количество слов должно быть от 2 до 30",
+                        "Ошибка",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error,
+                        MessageBoxDefaultButton.Button1);
+                return;
+            }
+            foreach (string word in words)
+            {
+                if (word.Length < 2 || word.Length > 30)
+                {
+                    MessageBox.Show(this,
+                        "В каждом слове должно быть от 2 до 30 букв",
+                        "Ошибка",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error,
+                        MessageBoxDefaultButton.Button1);
+                    return;
+                }
+            }
+
+            string last = words[words.Length - 1];  
+            string text = "";
+
+            foreach (string word in words)
+            {
+                if (word != last)
+                {
+                    char first = word[0];
+                    string new_word = first + word.Replace(first.ToString(), "");
+                    text += new_word + " ";
+                }
+            }
+            label2.Text = text;
+        }
+    }
+}
