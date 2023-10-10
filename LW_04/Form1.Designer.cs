04114ef5a316f4001f0a2038168baaa4b1d93ec8
rek655689 <e.gladneva1357@gmail.com>
2023-10-11 02:23:44 +0300

    отчёт по 4 лабе

--- LW_04/Form1.Designer.cs
+++ LW_04/Form1.Designer.cs
@@ -41,7 +41,7 @@
             this.главноеМенюToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(894, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(894, 30);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -55,16 +55,16 @@
             // panel1
             // 
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Location = new System.Drawing.Point(0, 30);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(894, 530);
+            this.panel1.Size = new System.Drawing.Size(894, 522);
             this.panel1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(894, 558);
+            this.ClientSize = new System.Drawing.Size(894, 552);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
