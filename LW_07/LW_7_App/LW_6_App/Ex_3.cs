c8d2ccd0425f712d0f06668dc36736d3e9f870cb
Lena-Zhuravleva <Elena-pionist@yandex.ru>
2023-11-04 22:51:36 +0300

    7 лаба

---
+++ LW_07/LW_7_App/LW_6_App/Ex_3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LW_6_App
+{
+    public partial class Ex_3 : UserControl
+    {
+        public Ex_3()
+        {
+            InitializeComponent();
+        }
+        void swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        void exercise_3(int count)
+        {
+            int elemnt_swap;
+            int[] massive = new int[count];
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                massive[i] = random.Next(20);
+                textBox2.Text += massive[i] + "  ";
+            }
+            elemnt_swap = Convert.ToInt32(textBox3.Text);
+            if (elemnt_swap == 1 || elemnt_swap > count)
+            {
+                MessageBox.Show("КРОМЕ 1 и числа больше количества элементов массива", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                elemnt_swap = Convert.ToInt32(textBox3.Text);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                if (i == elemnt_swap - 1)
+                {
+                    swap(ref massive[i], ref massive[i - 1]);
+                }
+            }
+            for (int i = 0; i < count; i++)
+            {
+                textBox4.Text += massive[i] + "  ";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox2.Clear();
+                textBox4.Clear();
+                int col_el = Convert.ToInt32(textBox1.Text);
+                if (col_el < 2)
+                {
+                    MessageBox.Show("Введите количество элементов в массиве больше 1", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    col_el = Convert.ToInt32(Console.ReadLine());
+                }
+                exercise_3(col_el);
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели символ или не ввели ничего, попробуйте ещё раз!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ LW_07/LW_7_App/LW_6_App/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace LW_6_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Главное меню";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Location = new System.Drawing.Point(4, 48);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(951, 526);
+            this.MainPanel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 586);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel MainPanel;
+    }
+}
+
