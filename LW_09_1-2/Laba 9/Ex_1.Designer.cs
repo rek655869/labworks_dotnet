737923d1a5c4163906fc5740aa52757c078ba80f
Nastya <anastasiagripunova@gmail.com>
2023-11-17 01:18:23 +0300

    ff

---
+++ LW_09_1-2/Laba 9/Ex_1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Laba_9
+{
+    partial class Ex_1
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(157, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(349, 111);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создать файл";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(12, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Последовательность чисел:";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(290, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(48, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Сумма нечетных чисел:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(290, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(236, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 31);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Файл создан!";
+            this.label5.Visible = false;
+            // 
+            // Ex_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Ex_1";
+            this.Size = new System.Drawing.Size(626, 387);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ LW_09_1-2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace _9_laba_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex_1();
+            Ex_2(); 
+        }
+        public static void Ex_1()
+        {
+            double x, sum = 0, k;
+            var rand = new Random();
+
+            try
+            {
+
+                using (FileStream File = new FileStream("file.bin", FileMode.OpenOrCreate))
+                {
+                    using (BinaryWriter writer = new BinaryWriter(File, Encoding.Default))
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            x = rand.Next(-100, 100) + rand.NextDouble();
+                            writer.Write(x);
+                            x = rand.Next(-100, 100);
+                            writer.Write(x);
+                        }
+                    }
+                }
+
+
+                using (FileStream File = new FileStream("file.bin", FileMode.Open))
+                {
+                    using (BinaryReader reader = new BinaryReader(File, Encoding.Default))
+                    {
+                        Console.Write("Последовательность чисел: ");
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            k = reader.ReadDouble();
+                            if (Math.Round(k) % 2 == -1 || Math.Round(k) % 2 == 1)
+                                sum += k;
+                            if (k == (int)k)
+                                Console.Write("{0}  ", k);
+                            else
+                                Console.Write("{0:0.00}  ", k);
+                        }
+
+                        Console.WriteLine("\nСумма нечётных чисел в файле - {0:0.00}", sum);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+        public static void Ex_2()
+        {
+            Console.Write("Введите число, которое хотите вставить в файл:\n > ");
+            var rand = new Random();
+            double k, y = 0;
+            y = num(y);
+            int offset = rand.Next(0, 10);
+            using (FileStream File = new FileStream("file.bin", FileMode.Open))
+            {
+                using (BinaryReader reader = new BinaryReader(File, Encoding.Default))
+                {
+                    //File.Seek(offset, SeekOrigin.Begin);
+                    //File.WriteByte(y);
+
+                    Console.Write("Последовательность чисел с новым числом: ");
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (i == offset)
+                            Console.Write("{0}  ", y);
+                        k = reader.ReadDouble();
+                        if (k == (int)k)
+                            Console.Write("{0}  ", k);
+                        else
+                            Console.Write("{0:0.00}  ", k);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+        public static double num(double s)
+        {
+
+            while (true)
+            {
+                string text = Console.ReadLine();
+                if (double.TryParse(text, out double number))
+                {
+
+                    Console.WriteLine("Вы ввели число {0}\n", number);
+                    s = double.Parse(text);
+                    break;
+                }
+                Console.WriteLine("Некорректное значение! Введите число.\n");
+            }
+            return s;
+
+        }
+    }
+}
+
+    
+
+    
+
