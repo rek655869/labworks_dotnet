148612d3a6f96640dee8af44204fdd6d4dc7ef5d
Lena-Zhuravleva <Elena-pionist@yandex.ru>
2023-11-19 23:50:41 +0300

    2 задание 10 лаба

---
+++ LW_10-2/LW_10/LW_10/Menu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LW_10
+{
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            Control panel = Parent;
+            switch (listBox1.SelectedIndex)
+                {
+                case 0:
+                    {
+                        Application.Exit();
+                        break;
+                    }
+                    
+                case 1:
+                    {
+                        UserControl us = new Ex_2();
+                        panel.Controls.Clear();
+                        panel.Controls.Add(us);
+                        us.Dock = DockStyle.Fill;
+                        break;
+                    }
+                case 3:
+                    {
+                        Application.Exit();
+                        break;
+                    }
+                case 4:
+                    {
+                        Application.Exit();
+                        break;
+                    }
+                case 5:
+                    {
+                        Application.Exit();
+                        break;
+                    }
+            }
+
+
+
+        }
+    }
+}
