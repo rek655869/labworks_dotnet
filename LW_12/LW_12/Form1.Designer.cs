e303570403b4b02f1910f224661936da4d70a543
Lena-Zhuravleva <Elena-pionist@yandex.ru>
2023-12-05 22:12:00 +0300

    Лабораторная 12

---
+++ LW_12/LW_12/Form1.Designer.cs
@@ -0,0 +1,444 @@
+﻿namespace LW_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.comboBoxExam3 = new System.Windows.Forms.ComboBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(192, 266);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox5.Location = new System.Drawing.Point(64, 234);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox4.Location = new System.Drawing.Point(64, 188);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox3.Location = new System.Drawing.Point(65, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox2.Location = new System.Drawing.Point(64, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(64, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 26);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Город\r\nпрописки";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Имя";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Номер";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Серия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Паспортные данные";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox11);
+            this.panel2.Controls.Add(this.textBox10);
+            this.panel2.Controls.Add(this.comboBoxExam3);
+            this.panel2.Controls.Add(this.textBox9);
+            this.panel2.Controls.Add(this.label17);
+            this.panel2.Controls.Add(this.comboBox6);
+            this.panel2.Controls.Add(this.label16);
+            this.panel2.Controls.Add(this.textBox8);
+            this.panel2.Controls.Add(this.textBox7);
+            this.panel2.Controls.Add(this.textBox6);
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Location = new System.Drawing.Point(270, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(488, 539);
+            this.panel2.TabIndex = 3;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Enabled = false;
+            this.textBox11.Location = new System.Drawing.Point(28, 228);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(121, 20);
+            this.textBox11.TabIndex = 23;
+            this.textBox11.Text = "Математика";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Enabled = false;
+            this.textBox10.Location = new System.Drawing.Point(28, 185);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(121, 20);
+            this.textBox10.TabIndex = 22;
+            this.textBox10.Text = "Русский язык";
+            // 
+            // comboBoxExam3
+            // 
+            this.comboBoxExam3.FormattingEnabled = true;
+            this.comboBoxExam3.Items.AddRange(new object[] {
+            "Физика",
+            "Информатика"});
+            this.comboBoxExam3.Location = new System.Drawing.Point(28, 267);
+            this.comboBoxExam3.Name = "comboBoxExam3";
+            this.comboBoxExam3.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxExam3.TabIndex = 21;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Enabled = false;
+            this.textBox9.Location = new System.Drawing.Point(119, 313);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(324, 20);
+            this.textBox9.TabIndex = 19;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(17, 313);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(77, 13);
+            this.label17.TabIndex = 18;
+            this.label17.Text = "Средний балл";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "Программист",
+            "Дизайнер",
+            "Радиотехник"});
+            this.comboBox6.Location = new System.Drawing.Point(101, 415);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(314, 21);
+            this.comboBox6.TabIndex = 17;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(205, 383);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(85, 13);
+            this.label16.TabIndex = 16;
+            this.label16.Text = "Специальность";
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox8.Location = new System.Drawing.Point(343, 260);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox7.Location = new System.Drawing.Point(343, 225);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox6.Location = new System.Drawing.Point(343, 181);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(9, 267);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(13, 13);
+            this.label15.TabIndex = 13;
+            this.label15.Text = "3";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(9, 228);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(13, 13);
+            this.label14.TabIndex = 11;
+            this.label14.Text = "2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(9, 188);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(377, 150);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(32, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Балл";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(60, 150);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Экзамен";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(175, 114);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(146, 26);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Экзамены, которые\r\nбыли выбраны на ЕГЭ/ОГЭ";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Основное общее (9 класс)",
+            "Среднее общее (11 классов)"});
+            this.comboBox2.Location = new System.Drawing.Point(218, 56);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(225, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(17, 56);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(73, 26);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "Уровень\r\nобразования";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(175, 9);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(132, 13);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "Данные об образовании";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(650, 639);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Сохранить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 670);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Поступление ВГТУ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.ComboBox comboBoxExam3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ LW_12/LW_12/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static LW_12.Form1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LW_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox6.DropDownStyle = ComboBoxStyle.DropDownList; //чтобы нельзя было писать в combobox
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxExam3.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        public interface IPassportInfo
+        {
+            int PassportNumber { get; set; }
+            int PassportSeries {get; set; }
+            string FirstName { get; set; }
+            string SecondName { get; set; }
+            string City { get; set; }
+        }
+        public class PassportInfo : IPassportInfo 
+        {
+            public int PassportNumber { get; set; }
+            public int PassportSeries { get; set; }
+            public string FirstName { get; set; }
+            public string SecondName { get; set; }
+            public string City { get; set; }
+        }
+
+        public interface IEducationLevel
+        {
+            string EducationType { get; set; }
+            int EducationYears { get; set; }
+        }
+        public class EducationLevel : IEducationLevel
+        {
+            public string EducationType { get; set; }
+            public int EducationYears { get; set; }
+        }
+
+        public interface IExamInfo
+        {
+            string ExamName { get; set; }
+            int Score { get; set; }
+        }
+        public class ExamInfo : IExamInfo
+        { 
+            public string ExamName { get; set; }
+            public int Score { get; set; } 
+        }
+
+        public interface ISpecialization 
+        {
+            string SpecializationName { get; set; }
+        }
+        public class Specialization : ISpecialization
+        {
+            public string SpecializationName { get; set; }
+        }
+
+        public class Abiturient : IPassportInfo, IEducationLevel, IExamInfo, ISpecialization
+        {
+            public int PassportNumber { get; set; }
+            public int PassportSeries { get; set; }
+            public string FirstName { get; set; }
+            public string SecondName { get; set; }
+            public string City { get; set; }
+            public string EducationType { get; set; }
+            public int EducationYears { get; set; }
+            public string ExamName { get; set; }
+            public int Score { get; set; }
+            public string SpecializationName { get; set; }
+
+
+
+        }
+        private bool ValidateScores()
+        {
+            int score1 = int.Parse(textBox6.Text);
+            int score2 = int.Parse(textBox7.Text);
+            int score3 = int.Parse(textBox8.Text);
+
+            if (score1 < 0 || score1 > 100 || score2 < 0 || score2 > 100 || score3 < 0 || score3 > 100)
+            {
+                MessageBox.Show("Введите баллы за экзамены от 0 до 100.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+        private void SaveAbiturientToFile(Abiturient applicant) //просто чтобы информация где-то хранилась
+        {
+            string filePath = "Abiturients.txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine("Паспортные данные:");
+                    writer.WriteLine($"Номер: {applicant.PassportNumber}");
+                    writer.WriteLine($"Серия: {applicant.PassportSeries}");
+                    writer.WriteLine($"Имя: {applicant.FirstName}");
+                    writer.WriteLine($"Город: {applicant.City}");
+                    writer.WriteLine();
+                    writer.WriteLine($"Уровень образования: {applicant.EducationType}");
+                    writer.WriteLine();
+                    writer.WriteLine($"Выбранные экзамены: {applicant.ExamName}");
+                    writer.WriteLine($"Средний балл: {applicant.Score:F2}");
+                    writer.WriteLine();
+                    writer.WriteLine($"Выбранная специальность: {applicant.SpecializationName}");
+                    writer.WriteLine("===============================================");
+                }
+
+                MessageBox.Show("Данные успешно сохранены в файл.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка сохранения данных: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                // Получение паспортных данных
+                int passportNumber = Convert.ToInt32(textBox1.Text);
+                int passportSeries = Convert.ToInt32(textBox2.Text);
+                string firstName = textBox3.Text;
+                string secondName = textBox4.Text;
+                string city = textBox5.Text;
+                int sum_exam;
+                string educationType = comboBox2.SelectedItem.ToString();
+                // Получение выбранных экзаменов
+                List<string> selectedExams = new List<string>
+                {
+                    textBox10.Text,
+                    textBox11.Text,
+                    comboBoxExam3.SelectedItem.ToString()
+                };
+                    // Получение баллов за экзамены
+                    List<int> scores = new List<int>
+                {
+                    int.Parse(textBox6.Text),
+                    int.Parse(textBox7.Text),
+                    int.Parse(textBox8.Text)
+
+                };
+                if (!ValidateScores())
+                    return;
+
+                sum_exam = (int.Parse(textBox6.Text) + int.Parse(textBox7.Text) + int.Parse(textBox8.Text)) / 3;
+                textBox9.Text = Convert.ToString(sum_exam);
+                // Получение выбранной специальности
+                string selectedSpecialization = comboBox6.SelectedItem.ToString();
+                // Создание абитуриента с полученными данными
+                Abiturient applicant = new Abiturient
+                {
+                    PassportNumber = passportNumber,
+                    PassportSeries = passportSeries,
+                    FirstName = firstName,
+                    SecondName = secondName,
+                    City = city,
+                    EducationType = educationType,
+                    ExamName = string.Join(", ", selectedExams),
+                    Score = sum_exam, // Вычисление среднего балла
+                    SpecializationName = selectedSpecialization
+
+                };
+                string message = $"Паспортные данные: \nНомер: {applicant.PassportNumber}\nСерия: {applicant.PassportSeries}\nИмя: {applicant.FirstName}\nГород: {applicant.City}\n\nУровень образования: {applicant.EducationType}\n\nВыбранные экзамены: {applicant.ExamName}\nСредний балл: {applicant.Score:F2}\n\nВыбранная специальность: {applicant.SpecializationName}";
+
+                MessageBox.Show(message, "Информация об абитуриенте");
+                SaveAbiturientToFile(applicant);
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели символ (в серии, номере, или баллах за экзамены) или не ввели ничего! Попробуйте еще раз!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
